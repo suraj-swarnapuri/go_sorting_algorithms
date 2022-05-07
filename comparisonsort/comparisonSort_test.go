21adb463701c626af0286f3834b85e7fd2ed23af
Suraj Swarnapuri <suraj-swarnapuri@github.com>
2022-05-06 20:32:27 -0500

    refactor test

--- comparisonsort/comparisonSort_test.go
+++ comparisonsort/comparisonSort_test.go
@@ -6,47 +6,48 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var (
+	intInput    = []int{10, 6, 4, 7, 3, 9, 1, 8, 2, 5}
+	floatInput  = []float64{10.0, 6.6, 4.4, 7.7, 3.3, 9.9, 1.1, 8.8, 2.2, 5.5}
+	stringInput = []string{"j", "i", "h", "g", "f", "e", "d", "c", "b", "a"}
+
+	expectedInt    = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	expectedString = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
+	expectedFloat  = []float64{1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.0}
+)
+
 func TestBubbleSortWithInt(t *testing.T) {
-	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	require.Equal(t, expected, BubbleSort(expected))
+	require.Equal(t, expectedInt, BubbleSort(intInput))
 }
 
 func TestBubbleSortWithString(t *testing.T) {
-	expected := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
-	require.Equal(t, expected, BubbleSort(expected))
+	require.Equal(t, expectedString, BubbleSort(stringInput))
 }
 
 func TestBubbleSortWithFloats(t *testing.T) {
-	expected := []float64{1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.0}
-	require.Equal(t, expected, BubbleSort(expected))
+	require.Equal(t, expectedFloat, BubbleSort(floatInput))
 }
 
 func TestInsertionSortWithInt(t *testing.T) {
-	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	require.Equal(t, expected, InsertionSort(expected))
+	require.Equal(t, expectedInt, InsertionSort(intInput))
 }
 
 func TestInsertionSortWithString(t *testing.T) {
-	expected := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
-	require.Equal(t, expected, InsertionSort(expected))
+	require.Equal(t, expectedString, InsertionSort(stringInput))
 }
 
 func TestInsertionSortWithFloats(t *testing.T) {
-	expected := []float64{1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.0}
-	require.Equal(t, expected, InsertionSort(expected))
+	require.Equal(t, expectedFloat, InsertionSort(floatInput))
 }
 
 func TestMergeSortWithInt(t *testing.T) {
-	expected := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	require.Equal(t, expected, MergeSort(expected))
+	require.Equal(t, expectedInt, MergeSort(intInput))
 }
 
 func TestMergeSortWithString(t *testing.T) {
-	expected := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"}
-	require.Equal(t, expected, MergeSort(expected))
+	require.Equal(t, expectedString, MergeSort(stringInput))
 }
 
 func TestMergeSortWithFloats(t *testing.T) {
-	expected := []float64{1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8, 9.9, 10.0}
-	require.Equal(t, expected, MergeSort(expected))
+	require.Equal(t, expectedFloat, MergeSort(floatInput))
 }
